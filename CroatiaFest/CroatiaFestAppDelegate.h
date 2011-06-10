0e99425abb4cdc3bf0ec2a5ce92bac2994aafcbd
Lori Hill <frostbitelearning@gmail.com>
2011-06-10 08:54:56 -0700

    Initial Commit

---
+++ CroatiaFest/CroatiaFestAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CroatiaFestAppDelegate.h
+//  CroatiaFest
+//
+//  Created by Lori Hill on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CroatiaFestAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ CroatiaFest/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  CroatiaFest
+//
+//  Created by Lori Hill on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
